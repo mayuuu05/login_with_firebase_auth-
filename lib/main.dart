214c14c523450dcbcbb8d04c94e264a73b8d0cb4
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-09-13 19:27:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'google_signIn/view/google_auth_manager.dart';
+import 'google_signIn/view/google_sign_in.dart';
+import 'google_signIn/view/google_sign_up.dart';
+import 'google_signIn/view/home_screen.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -18,6 +23,17 @@ class ChatApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       getPages: [
+        //LOGIN AND SIGN UP
+        // GetPage(name: '/', page: () => const AuthManager(),),
+        // GetPage(name: '/signIn', page: () => const SignIn(),),
+        // GetPage(name: '/signUp', page: () => const SignUp(),),
+        // GetPage(name: '/home', page: () => const HomePage(),),
+
+        // GOOGLE SIGNIN
+        GetPage(name: '/', page: () =>  GoogleAuthManager(),),
+        GetPage(name: '/signup', page: () =>  SignUpPage(),),
+        GetPage(name: '/login', page: () =>  SignInPage(),),
+        GetPage(name: '/home', page: () =>  HomeScreen(),),
       ],
     );
   }
