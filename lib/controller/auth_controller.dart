ed416f84b7003af18a506598bd705693738008b6
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-09-13 19:29:04 +0530

    Delete lib/controller directory

--- lib/controller/auth_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
-
-class AuthController  extends GetxController{
-
-  TextEditingController txtEmail = TextEditingController();
-  TextEditingController txtPassword = TextEditingController();
-
-}
